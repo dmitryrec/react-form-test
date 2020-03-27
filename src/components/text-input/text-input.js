91c86ccdf1f81b4a18adb91012f6dfdded3c52e6
unknown <recfrag@gmail.com>
2020-03-27 21:10:00 +0300

    fix css

--- src/components/text-input/text-input.js
+++ src/components/text-input/text-input.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import './text-input.css';
 
 export default function TextInput({text}) {
     return (
         <input
+            className="text-input"
             type="text" 
             value={text} 
             placeholder="Text field" 
