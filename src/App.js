dbd1edc3ac8e272dc63471dbcebeb016e3381ec5
unknown <recfrag@gmail.com>
2020-03-27 20:05:15 +0300

    add header and some styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { data } from './data';
 import TextInput from './components/text-input/text-input';
 import SelectInput from './components/select-input/select-input';
+import Header from './components/header/header';
 
 export default class App extends Component {
 
@@ -22,13 +23,21 @@ export default class App extends Component {
   };
 
   render () {
+    const { text1, text2, text3 } = this.state;
+
     return (
-      <form>
-        <SelectInput {...data}/>
-        <TextInput onChange={this.handleInputChange} value={this.state.text1}/>
-        <TextInput onChange={this.handleInputChange} value={this.state.text2}/>
-        <TextInput onChange={this.handleInputChange} value={this.state.text3}/>
-      </form>
+      <div className='container'>
+        <Header/>
+        <form className='form-container'>
+          <SelectInput {...data}/>
+          <TextInput onChange={this.handleInputChange} value={text1}/>
+          <TextInput onChange={this.handleInputChange} value={text2}/>
+          <TextInput onChange={this.handleInputChange} value={text3}/>
+          <button>Reset</button>
+          <button>Filter</button>
+        </form>
+      </div>
+      
     );
   }
   
