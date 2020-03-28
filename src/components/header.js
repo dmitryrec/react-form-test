f28c23b23447ee77b0df72052b448f84a368cbee
unknown <recfrag@gmail.com>
2020-03-28 12:27:27 +0300

    fix styles

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <h1>New Page</h1>
+        <>
+            <h1>New Page</h1>
+            <img src='https://cdn4.iconfinder.com/data/icons/pictype-free-vector-icons/16/home-512.png' width='5%' alt='home'/>
+        </>
     )
 }
